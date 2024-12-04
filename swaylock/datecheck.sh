72d640b0f5d25127b4251863e76e15e89c90904d
poseidonai555 <poseidonai555@gmail.con>
2024-12-04 16:21:55 +0000

    added local scripts and added a script to correct spacing in date string for swaylock

---
+++ swaylock/datecheck.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Get the current day of the month
+day=$(date +%d)
+
+# Define the path to the config file
+config_file="/home/poseidon/.config/swaylock/config"
+
+# Check if the day is a single digit
+if [[ $day -lt 10 ]]; then
+  # If single digit, remove space between %B and %e
+  sed -i 's/datestr=%a, %B %e/datestr=%a, %B%e/' "$config_file"
+else
+  # If two digits, add space between %B and %e
+  sed -i 's/datestr=%a, %B%e/datestr=%a, %B %e/' "$config_file"
+fi
